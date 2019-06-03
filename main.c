2aaf647fa6a6d3680f9f4c2b951ef743fba5e7fd
cydauto <51304653+cydauto@users.noreply.github.com>
2019-06-03 09:37:03 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+  int i,j,sum=0;
+  i=8;
+  j=6;
+  printf("helloworld:%d,%x",i,j);
+  while(1)
+  {
+  
+  
+  }
+}
